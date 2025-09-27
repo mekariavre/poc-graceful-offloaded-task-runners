54021e70d464ca2411aeeb2a1d8c73d58652a118
Avre Barra <avreghly.barra@mekari.com>
2025-09-27 09:02:40 +0700

    add scenario: basic usage

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/mekariavre/poc-graceful-offloaded-task-runners
 
 go 1.22.12
+
+require (
+	github.com/panjf2000/ants/v2 v2.11.3 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,35 @@
 package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/panjf2000/ants/v2"
+)
+
+func main() {
+	t0 := time.Now()
+
+	fmt.Println("starting...")
+	scenario_1()
+	fmt.Printf("exited (%s)\n", time.Since(t0))
+}
+
+func scenario_1() {
+	fmt.Println("scenario: basic usage")
+	// create a pool with 10 workers
+	pool, _ := ants.NewPool(10)
+	defer pool.Release()
+
+	// submit tasks
+	for i := 0; i < 20; i++ {
+		idx := i
+		_ = pool.Submit(func() {
+			fmt.Printf("Task %d running\n", idx)
+			time.Sleep(time.Second)
+		})
+	}
+
+	// wait so we can see output before exit
+	time.Sleep(3 * time.Second)
+}
