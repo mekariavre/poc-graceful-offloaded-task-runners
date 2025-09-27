79d4fa9d295bab6fa46ad58c0e0c4903aad368e5
Avre Barra <avreghly.barra@mekari.com>
2025-09-27 09:12:09 +0700

    add scenario: without waiting & non-blocking
    it immediately unblock when run with nonblocking options

--- main.go
+++ main.go
@@ -12,8 +12,32 @@ func main() {
 	t0 := time.Now()
 
 	fmt.Println("starting...")
-	scenario_2()
+	scenario_3()
 	fmt.Printf("exited (%s)\n", time.Since(t0))
+}
+
+func scenario_3() {
+	fmt.Println("scenario: without waiting & non-blocking")
+	// create a pool with 5 workers
+	pool, _ := ants.NewPool(5, ants.WithNonblocking(true))
+	defer pool.Release()
+
+	// submit tasks non-blocking, ignore error if pool is full
+	for i := 0; i < 20; i++ {
+		idx := i
+		_ = pool.Submit(func() {
+			time.Sleep(5000 * time.Millisecond)
+			log.Printf("Task %d done\n", idx)
+		})
+	}
+
+	// immediately return, not waiting for tasks
+	log.Println("main function continues immediately")
+
+	// starting...
+	// scenario: without waiting & non-blocking
+	// 2025/09/27 09:11:07 main function continues immediately
+	// exited (128.75µs)
 }
 
 func scenario_2() {
