e5d42e7a467b2311bd9ed592ccb733c5accd217b
Avre Barra <avreghly.barra@mekari.com>
2025-09-27 09:08:12 +0700

    add scenario: without waiting
    apparently it still blocks

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/panjf2000/ants/v2"
@@ -11,8 +12,27 @@ func main() {
 	t0 := time.Now()
 
 	fmt.Println("starting...")
-	scenario_1()
+	scenario_2()
 	fmt.Printf("exited (%s)\n", time.Since(t0))
+}
+
+func scenario_2() {
+	fmt.Println("scenario: without waiting")
+	// create a pool with 5 workers
+	pool, _ := ants.NewPool(5)
+	defer pool.Release()
+
+	// submit tasks
+	for i := 0; i < 20; i++ {
+		idx := i
+		_ = pool.Submit(func() {
+			time.Sleep(5000 * time.Millisecond)
+			log.Printf("Task %d done\n", idx)
+		})
+	}
+
+	// doing other stuff
+	log.Println("doing other stuff")
 }
 
 func scenario_1() {
